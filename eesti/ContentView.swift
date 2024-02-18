522a7d7074bbacbda83a181ca30d06fc34013868
Taha Tesser <tessertaha@gmail.com>
2024-02-18 21:05:05 +0200

    Initial Commit

---
+++ eesti/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  eesti
+//
+//  Created by Taha Tesser on 18.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ eesti/eestiApp.swift
@@ -0,0 +1,17 @@
+//
+//  eestiApp.swift
+//  eesti
+//
+//  Created by Taha Tesser on 18.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct eestiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
